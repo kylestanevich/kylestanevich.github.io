6150c1769f68c874894275fafa312caf82df5943
kylestanevich <kylestanevich@gmail.com>
2020-05-16 14:27:41 -0700

    added newnew testing
    attempting to do only css (no js)

--- newnewsite/js/js.js
+++ newnewsite/js/js.js
@@ -42,50 +42,7 @@ function resetheight(item){
 	item.style.height = "auto";
 }
 
-function maps() {
-	//-----map start-----//
-	// create a LatLng object containing the coordinate for the center of the map
-	var latlng = new google.maps.LatLng(36.9439079,-103.01245395);
-	// create other objects containing the coordinates for other locations
-	var cahome = new google.maps.LatLng(33.7428704,-117.7764242);
-	var ilhome = new google.maps.LatLng(40.1449454,-88.2484837); 
-	// prepare the map properties
-	var options = {
-		zoom: 4,
-		center: latlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		navigationControl: true,
-		mapTypeControl: false,
-		scrollwheel: false,
-		disableDoubleClickZoom: true
-	};
-	// initialize the map object
-	var map = new google.maps.Map(document.getElementById('map'), options);
-	// add Markers
-	var marker1 = new google.maps.Marker({
-		position: cahome, map: map
-	});
-	var marker2 = new google.maps.Marker({
-		position: ilhome, map: map
-	});
-	// add listener for a click on the cahome pin
-	google.maps.event.addListener(marker1, 'click', function() {
-		infowindow1.open(map, marker1);
-	});
-	// add listener for a click on the ilhome pin
-	google.maps.event.addListener(marker2, 'click', function() {
-		infowindow2.open(map, marker2);
-	});
-	// add information window for cahome
-	var infowindow1 = new google.maps.InfoWindow({
-		content:  '<div class="info"><strong>Where I reside when not at school.</strong><br><br>Tustin California</div>'
-	});
-	// add information window for ilhome
-	var infowindow2 = new google.maps.InfoWindow({
-		content:  '<div class="info"><strong>Where I reside when at school.</strong><br><br>Champaign Illinois</div>'
-	});
-	//-----map end-----//
-}
+
 
 function getinitsize(){
 	var padding = parseInt(window.getComputedStyle(document.querySelector(".topnav")).getPropertyValue('padding'),10);
@@ -137,9 +94,7 @@ function navbarupdate(){
 	}
 }
 
-function say(){
-	alert("1");
-}
+
 
 
 
@@ -148,29 +103,6 @@ var maxheight;
 var criticalBodyWidth;
 
 
-document.onclick = function(e) {
-	e = e.target;
-	if (e.className && e.className.indexOf('modaltrig') != -1) {
-		var modal = document.getElementById('modal'+e.id);
-		modal.style.display = "block";
-	}
-	else if (e.parentElement.className && e.parentElement.className.indexOf('modaltrig') != -1) {
-		var modal = document.getElementById('modal'+e.id);
-		modal.style.display = "block";
-	}
-	else if (e.className && e.className.indexOf('close') != -1) {
-		var modal = document.getElementById(e.parentElement.parentElement.id);
-		if (modal != null) {
-			modal.style.display = "none";
-		}
-	}
-	else if (e.className && e.className.indexOf('modal') != -1) {
-		var modal = document.getElementById(e.id);
-		if (modal != null) {
-			modal.style.display = "none";
-		}
-	}
-}
 
 window.onresize = resize;
 
